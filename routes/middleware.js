0c8da3426fe7c444df194353e9ff28fcb4a4d5d3
ryancwynar <rtcx86@gmail.com>
2016-11-08 00:35:02 -0800

    changed home page

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,9 +19,8 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'Main', key: 'home', href: '/' },
+		{ label: 'Projects', key: 'blog', href: '/blog' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
